2019e862426c910348a2b9c6c2beef2956dfc08b
Pavel <pavel.chernenko97@gmail.com>
2022-07-22 01:05:12 +0300

    add task 1

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+// Normal lvl
+    
+    // task 1
+    let a = true;
+    let b = false;
+    let c = 17;
+    let d = undefined;
+    let e = null;
+    console.log(`a : ${typeof(a)},
+        b: ${typeof(b)}, 
+        c: ${typeof(c)}, 
+        d: ${typeof(d)}, 
+        e: ${typeof(e)}`)
+
